3af9341fc322d384d1b0dde3df737e91dc4e5180
Henry <omgredstone@gmail.com>
2019-04-01 23:01:15 +0100

    Rewriting for Sequence - websockets + scene interface + users interface works

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
+const http = require("http");
 const express = require("express");
 const session = require("express-session");
+const bodyParser = require("body-parser");
 const Grant = require("grant-express");
-const ws = require("uws");
+const ws = require("@clusterws/cws");
 const PostgresStore = require("connect-pg-simple")(session);
 
 const config = require("./lib/config");
@@ -33,6 +30,8 @@ const grantOpts = {
 
 const grant = new Grant(grantOpts);
 const app = express();
+const server = http.createServer(app);
+const wss = new ws.WebSocketServer({ server });
 
 app.set("view engine", "pug");
 
@@ -46,7 +45,18 @@ app.use(session({
 );
 app.use(grant);
 
-app.get("/auth/callback", function(req, res) {
+app.get("/v1/targets", (req, res) =>
+    db.getAllScenes().then(scenes =>
+        res.send({
+            "targets": scenes.map(s => {
+                let a = s.scene_id.split("_")
+                return { chapter: parseInt(a[0]), scene: parseInt(a[1]), fullname: s.fullname };
+            })
+        })
+    ).catch(err => res.send({ "error": "problem retrieving the scenes :(" }))
+);
+
+app.get("/auth/callback", function(req, res, next) {
     const data = req.query;
     req.session.user = {
         access_token: data.access_token,
@@ -58,8 +68,14 @@ app.get("/auth/callback", function(req, res) {
         const re = new reddit.Client(data.access_token, data.refresh_token);
         return re.getUserInfo().then(u => db.insertUser(u.id, u.name, data.access_token, data.refresh_token));
     });
-    
-    return res.redirect(config.loggedInRedirect);
+
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        
+        return res.redirect(config.loggedInRedirect);
+    })
 });
 
 app.use(function(req, res, next) {
@@ -82,7 +98,9 @@ app.use("/admin", function(req, res, next) {
                 return next();
             } else {
                 return res.status(401).send("You're not an administrator!");
-            }}).catch(err => next(err));}).catch(err => next(err));
+            }
+        }).catch(err => next(err));
+    }).catch(err => res.redirect("/connect/reddit"));
 });
 
 app.get("/admin/dash", (req, res) =>
@@ -95,13 +113,44 @@ app.get("/admin/dash", (req, res) =>
 );
 
 app.get("/admin/dash/users", (req, res) =>
-    db.getAllUsers().then(users => res.render("admin-users.pug", {users}))
+    db.getAllUsers().then(users => res.render("admin-users.pug", { users }))
 );
 
-app.get("/admin/dash/circles", (req, res) => res.render("admin-circles.pug"));
+app.get("/admin/dash/scenes", (req, res) =>
+    db.getAllScenes().then(scenes => res.render("admin-circles.pug", { scenes }))
+);
 
-app.delete("/admin/dash/users/:id", (req, res) =>
-    db.deleteUser(req.params.id).then(() => res.send({success: true})).catch(err => res.send({success: false}))
+app.post("/admin/dash/scenes/:id", bodyParser.urlencoded(extended: false), (req, res, next) =>
+    db.addSceneItem(req.body.sceneId, req.body.fullname)
+        .then(() => {
+            wss.broadcast(JSON.stringify({ "type": "new-scene", "fullname": req.body.fullname }));
+            
+            res.redirect("/admin/dash/scenes");
+        }).catch(err => next(err))
+);
+
+app.get("/admin/dash/scenes/:id/delete", (req, res, next) =>
+    db.deleteScene(req.params.id)
+        .then(() => res.redirect("/admin/dash/scenes"))
+        .catch(err => next(err))
+);
+
+app.get("/admin/dash/users/:id/make_admin", (req, res, next) =>
+    db.setUserAdmin(req.params.id, true)
+        .then(() => res.redirect("/admin/dash/users"))
+        .catch(err => next(err))
+);
+
+app.get("/admin/dash/users/:id/delete", (req, res, next) =>
+    db.deleteUser(req.params.id)
+        .then(() => res.redirect("/admin/dash/users"))
+        .catch(err => next(err))
+);
+
+app.get("/admin/dash/users/:id/revoke_admin", (req, res, next) =>
+    db.setUserAdmin(req.params.id, false)
+        .then(() => res.redirect("/admin/dash/users"))
+        .catch(err => next(err))
 );
 
 app.get("/admin/info", (req, res, next) => res.send(req.user));
@@ -114,4 +163,22 @@ app.use(function(err, req, res, next) {
     return res.render("error.pug", {error: err});
 });
 
-app.listen(4003);
+function handleSocketMessage(msg) {
+    if (msg.type == "science") {
+        // todo: read params
+    }
+}
+
+wss.on("connection", (socket) => {
+    db.getAllScenes().then(scenes =>
+        socket.send(
+            JSON.stringify({ "type": "scenes", "fullnames": scenes.map(s => s.fullname) })
+        )
+    );
+
+    socket.on("message", (data) =>
+        handleSocketMessage(JSON.parse(data))
+    );
+});
+
+server.listen(4003);
